2358e5c59ad09e85281fac5fc37edcf4a3282fd6
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-04-11 16:10:32 +0300

    feat: add solution mine-sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,27 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const res = [];
+  let cell = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    res.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      cell = 0;
+      if (j + 1 < matrix[i].length && matrix[i][j + 1] === true) cell++;
+      if (i + 1 < matrix.length && j + 1 < matrix[i].length && matrix[i + 1][j + 1] === true) {
+        cell++;
+      }
+      if (i + 1 < matrix.length && matrix[i + 1][j] === true) cell++;
+      if (i + 1 < matrix.length && j - 1 > -1 && matrix[i + 1][j - 1] === true) cell++;
+      if (j - 1 > -1 && matrix[i][j - 1] === true) cell++;
+      if (i - 1 > -1 && j - 1 > -1 && matrix[i - 1][j - 1] === true) cell++;
+      if (i - 1 > -1 && matrix[i - 1][j] === true) cell++;
+      if (i - 1 > -1 && j + 1 < matrix[i].length && matrix[i - 1][j + 1] === true) cell++;
+      res[i].push(cell);
+    }
+  }
+  return res;
 }
 
 module.exports = minesweeper;
