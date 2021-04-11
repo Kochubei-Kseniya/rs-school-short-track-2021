99ebbc8c37d0998d49722dacf9edc3d9203183bf
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-04-11 21:32:48 +0300

    Update 03-sort-by-height.js

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,7 +9,7 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(arr) {
+/*function sortByHeight(arr) {
   const res = [];
 
   const sorted = [...arr].sort((a, b) => a - b).filter((val) => val !== -1);
@@ -17,6 +17,13 @@ function sortByHeight(arr) {
   arr.map((value) => (value === -1 ? res.push(-1) : res.push(sorted.shift())));
 
   return res;
+}
+
+module.exports = sortByHeight;
+*/
+
+function sortByHeight(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = sortByHeight;
