99994e17a5b8e3076b59f470b8ee944381adcf52
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-04-12 01:07:04 +0300

    feat: 7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -1,20 +1,17 @@
-/**
- * There's a list of file, since two files cannot have equal names,
- * the one which comes later will have a suffix (k),
- * where k is the smallest integer such that the found name is not used yet.
- *
- * Return an array of names that will be given to the files.
- *
- * @param {Array} names
- * @return {Array}
- *
- * @example
- * For input ["file", "file", "image", "file(1)", "file"],
- * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
- */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const arr = [...names];
+
+  for (let i = 0; i < arr.length; i++) {
+    let count = 1;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        arr[j] = `${arr[j]}(${count})`;
+        count++;
+      }
+    }
+  }
+
+  return arr;
 }
 
 module.exports = renameFiles;
