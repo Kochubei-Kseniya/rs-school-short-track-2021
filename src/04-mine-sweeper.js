1332e9435f0f6fb1f0ca68f8c77a4e92e2878902
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-04-12 00:38:03 +0300

    feat: 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,21 +1,29 @@
 function minesweeper(matrix) {
   const result = [];
-  const count = 0;
+  let cell = 0;
   for (let i = 0; i < matrix.length; i++) {
     result.push([]);
     for (let j = 0; j < matrix[i].length; j++) {
-      count = 0;
-      if (j + 1 < matrix[i].length && matrix[i][j + 1] === true) count++;
+      cell = 0;
+      // right
+      if (j + 1 < matrix[i].length && matrix[i][j + 1] === true) cell++;
+      // bottom right
       if (i + 1 < matrix.length && j + 1 < matrix[i].length && matrix[i + 1][j + 1] === true) {
-        count++;
-      }      
-      if (i + 1 < matrix.length && matrix[i + 1][j] === true) count++;      
-      if (i + 1 < matrix.length && j - 1 > -1 && matrix[i + 1][j - 1] === true) count++;      
-      if (j - 1 > -1 && matrix[i][j - 1] === true) count++;     
-      if (i - 1 > -1 && j - 1 > -1 && matrix[i - 1][j - 1] === true) count++;    
-      if (i - 1 > -1 && matrix[i - 1][j] === true) count++;    
-      if (i - 1 > -1 && j + 1 < matrix[i].length && matrix[i - 1][j + 1] === true) count++;
-      result[i].push(count);
+        cell++;
+      }
+      // bottom
+      if (i + 1 < matrix.length && matrix[i + 1][j] === true) cell++;
+      // bottom left
+      if (i + 1 < matrix.length && j - 1 > -1 && matrix[i + 1][j - 1] === true) cell++;
+      // left +
+      if (j - 1 > -1 && matrix[i][j - 1] === true) cell++;
+      // top left
+      if (i - 1 > -1 && j - 1 > -1 && matrix[i - 1][j - 1] === true) cell++;
+      // top
+      if (i - 1 > -1 && matrix[i - 1][j] === true) cell++;
+      // top right
+      if (i - 1 > -1 && j + 1 < matrix[i].length && matrix[i - 1][j + 1] === true) cell++;
+      result[i].push(cell);
     }
   }
   return result;
