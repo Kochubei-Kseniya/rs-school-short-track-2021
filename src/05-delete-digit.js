6c9fdfcad4a98576283e8998ef393018872c76a0
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-04-12 00:51:23 +0300

    feat:5

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -1,5 +1,14 @@
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let max = 0;
+  const arr = String(n).split('');
+  const length = arr.length;
+  const newArr = (i) => Number(arr.slice(0, i).concat(arr.slice(i + 1, length)).join(''));
+  for (let i = 0; i < length; i++) {
+    if (max < newArr(i)) {
+      max = newArr(i);
+    }
+  }
+  return max;
 }
 
 module.exports = deleteDigit;
