896746e84160b031c4713af09c77be31ac10af58
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-04-12 00:20:51 +0300

    feat: 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,5 +1,24 @@
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      count = 0;
+      if (j + 1 < matrix[i].length && matrix[i][j + 1] === true) count++;
+      if (i + 1 < matrix.length && j + 1 < matrix[i].length && matrix[i + 1][j + 1] === true) {
+        count++;
+      }      
+      if (i + 1 < matrix.length && matrix[i + 1][j] === true) count++;      
+      if (i + 1 < matrix.length && j - 1 > -1 && matrix[i + 1][j - 1] === true) count++;      
+      if (j - 1 > -1 && matrix[i][j - 1] === true) count++;     
+      if (i - 1 > -1 && j - 1 > -1 && matrix[i - 1][j - 1] === true) count++;    
+      if (i - 1 > -1 && matrix[i - 1][j] === true) count++;    
+      if (i - 1 > -1 && j + 1 < matrix[i].length && matrix[i - 1][j + 1] === true) count++;
+      result[i].push(count);
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
