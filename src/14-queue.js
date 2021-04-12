e6b300373291899b454e72684e7e7798593450e2
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-04-12 11:16:10 +0300

    feat: 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -12,16 +12,39 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.length = 0;
+    this.head = null;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.length;
+  }
+
+  enqueue(element) {
+    const newNode = new ListNode(element);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    let currentNode = this.head;
+    if (currentNode) {
+      const val = currentNode.value;
+      currentNode = currentNode.next;
+      this.head = currentNode;
+      this.length--;
+      return val;
+    }
+    return null;
   }
 }
 
