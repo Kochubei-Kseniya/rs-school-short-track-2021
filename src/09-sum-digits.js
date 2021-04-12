29447290747ea876daac89e05020601a7d81a1be
Kochubei Kseniya <79758262+Kochubei-Kseniya@users.noreply.github.com>
2021-04-12 08:30:46 +0300

    feat: add solution 9

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -1,28 +1,16 @@
-/**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
- *
- */
 function getSumOfDigits(n) {
   const newArr = n.toString().split('');
   const digitArr = [];
   newArr.forEach((el) => digitArr.push(Number(el)));
 
-  function getSum(arr) {
+  function getArrSum(arr) {
     let digitSum = 0;
     for (let i = 0; i < arr.length; i++) {
       digitSum += arr[i];
     }
     return digitSum;
   }
-  let sum = getSum(digitArr);
+  let sum = getArrSum(digitArr);
   if (sum > 9) {
     sum = getSumOfDigits(sum);
   }
